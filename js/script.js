13d3688b86dfc9e9fcdf59c0c4f011cae4e00126
unknown <joseph.scalera1@gmail.com>
2020-11-01 21:08:56 -0500

    Finished Main Structure

--- js/script.js
+++ js/script.js
@@ -1,3 +1,28 @@
+restaurants = [
+    {
+        "name": "O'Begal",
+        "address": "600 Washington St, Hoboken, NJ 07030",
+        "categories": [
+            "Bagel", "Takeout"
+        ]
+    },
+    {
+        "name": "Jefferson’s Coffee",
+        "address": "534 Washington St, Hoboken, NJ 07030",
+        "categories": [
+            "Coffee", "Takeout"
+        ]
+    },
+    {
+        "name": "Ayame Hibachi & Sushi",
+        "address": "526 Washington St, Hoboken, NJ 07030",
+        "categories": [
+            "Sushi", "Takeout"
+        ]
+    }
+]
+
+
 //Returns: Array of strings
 //Purpose: Parse the individual words in the given string
 //"Hello World" --> {"Hello", "World"}
@@ -29,29 +54,6 @@ function getInput(){
     string = document.getElementById("SearchBar").value;
     stringArray = parseInput(string);
     
-    restaurants = [
-        {
-            "name": "O'Begal",
-            "address": "600 Washington St, Hoboken, NJ 07030",
-            "categories": [
-                "Bagel", "Takeout"
-            ]
-        },
-        {
-            "name": "Jefferson’s Coffee",
-            "address": "534 Washington St, Hoboken, NJ 07030",
-            "categories": [
-                "Coffee", "Takeout"
-            ]
-        },
-        {
-            "name": "Ayame Hibachi & Sushi",
-            "address": "526 Washington St, Hoboken, NJ 07030",
-            "categories": [
-                "Sushi", "Takeout"
-            ]
-        }
-    ]
     indexArray = []
     for (let i = 0; i < stringArray.length; i++){
         let thisString = stringArray[i];
@@ -71,6 +73,29 @@ function getInput(){
     for (let i = 0; i < indexArray.length; i++){
         elmName = restaurants[indexArray[i]].name;
         elmAddress = restaurants[indexArray[i]].address;
+
+        // Create an empty <tr> element and add it to the 1st position of the table:
+        var row = SearchResults.insertRow(0);
+
+        // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+
+        // Add some text to the new cells:
+        cell1.innerHTML = elmName;
+        cell2.innerHTML = elmAddress;
+    }
+}
+
+function loadAllRestaurants(){
+    console.log(1);
+    SearchResults = document.getElementById("SearchResults");
+    while(SearchResults.hasChildNodes()){
+        SearchResults.removeChild(SearchResults.firstChild);
+    }
+    for (let i = 0; i < restaurants.length; i++){
+        elmName = restaurants[i].name;
+        elmAddress = restaurants[i].address;
 
         // Create an empty <tr> element and add it to the 1st position of the table:
         var row = SearchResults.insertRow(0);
