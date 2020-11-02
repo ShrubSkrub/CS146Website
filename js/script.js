09afc8a3a23d8a5f3b6e9baadef9fd41b00c1e81
unknown <joseph.scalera1@gmail.com>
2020-11-01 20:56:44 -0500

    Finished Main Structure

--- js/script.js
+++ js/script.js
@@ -11,6 +11,19 @@ function isSpace(str){
     return str != " ";
 }
 
+function checkInJSON(str, index){
+    if (str == restaurants[index].name){
+        return true;
+    }
+
+    for (let i = 0; i < restaurants[index].categories.length; i++){
+        if (str == restaurants[index].categories[i]){
+            return true;
+        }
+    }
+    return false;
+}
+
 function getInput(){
 
     string = document.getElementById("SearchBar").value;
@@ -21,14 +34,14 @@ function getInput(){
             "name": "O'Begal",
             "address": "600 Washington St, Hoboken, NJ 07030",
             "categories": [
-                "Bagel"
+                "Bagel", "Takeout"
             ]
         },
         {
             "name": "Jefferson’s Coffee",
             "address": "534 Washington St, Hoboken, NJ 07030",
             "categories": [
-                "Coffee"
+                "Coffee", "Takeout"
             ]
         },
         {
@@ -43,13 +56,31 @@ function getInput(){
     for (let i = 0; i < stringArray.length; i++){
         let thisString = stringArray[i];
         for (let j = 0; j < restaurants.length; j++){
-            if (thisString == restaurants[j].name){
+            if (checkInJSON(thisString, j)){
                 indexArray.push(j);
             }
         }
     }
 
+
+    SearchResults = document.getElementById("SearchResults");
+    while(SearchResults.hasChildNodes()){
+        SearchResults.removeChild(SearchResults.firstChild);
+    }
+    
     for (let i = 0; i < indexArray.length; i++){
-        console.log(restaurants[i].name);
+        elmName = restaurants[indexArray[i]].name;
+        elmAddress = restaurants[indexArray[i]].address;
+
+        // Create an empty <tr> element and add it to the 1st position of the table:
+        var row = SearchResults.insertRow(0);
+
+        // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+
+        // Add some text to the new cells:
+        cell1.innerHTML = elmName;
+        cell2.innerHTML = elmAddress;
     }
 }
