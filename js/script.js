927afe5b142908f03ef0299524fbf2351380dd87
unknown <joseph.scalera1@gmail.com>
2020-11-02 20:01:50 -0500

    11/2

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 restaurants = [
     {
-        "name": "O'Begal",
+        "name": "O'Bagel",
         "address": "600 Washington St, Hoboken, NJ 07030",
         "categories": [
-            "Bagel", "Takeout"
+            "Bagel"
         ]
     },
     {
         "name": "Jefferson’s Coffee",
         "address": "534 Washington St, Hoboken, NJ 07030",
         "categories": [
-            "Coffee", "Takeout"
+            "Coffee"
         ]
     },
     {
@@ -19,9 +19,253 @@ restaurants = [
         "categories": [
             "Sushi", "Takeout"
         ]
+    },
+    {
+        "name": "Benny Tudino’s",
+        "address": "622 Washington St, Hoboken, NJ 07030",
+        "categories": [
+            "Pizza", "Takeout", "Cash Only"
+        ]
+    },
+    {
+        "name": "The Madison Bar & Grill",
+        "address": "1316 Washington St, Hoboken, NJ 07030",
+        "categories": [
+            "Bar", "Takeout", "Grill"
+        ]
+    },
+    {
+        "name": "Bin 14",
+        "address": "1314 Washington St, Hoboken, NJ 07030",
+        "categories": [
+            "Wine", "Takeout", "Bar", "Dine"
+        ]
+    },
+    {
+        "name": "Apulia",
+        "address": "1319 Washington St, Hoboken, NJ 07030",
+        "categories": [
+            "Pizza", "Takeout", "Lunch", "Dine"
+        ]
+    },
+    {
+        "name": "Stingray Lounge",
+        "address": "1210 Washington St, Hoboken, NJ 07030",
+        "categories": [
+            "Cocktail", "Takeout", "Bar", "Dine", "Outdoor"
+        ]
+    },
+    {
+        "name": "Green Garden",
+        "address": "1202 Washington St, Hoboken, NJ 07030",
+        "categories": [
+            "Chinese", "Takeout", "Comfort", "Dine"
+        ]
+    },
+    {
+        "name": "The Brick",
+        "address": "1122 Washington St, Hoboken, NJ 07030",
+        "categories": [
+            "Pizza", "Takeout", "Comfort", "Dine"
+        ]
+    },
+    {
+        "name": "Yeung II",
+        "address": "1120 Washington St, Hoboken, NJ 07030",
+        "categories": [
+            "Chinese", "Takeout"
+        ]
+    },
+    {
+        "name": "Napoli’s Pizza",
+        "address": "1118 Washington St, Hoboken, NJ 07030",
+        "categories": [
+            "Pizza", "Takeout", "Dine", "Outdoor"
+        ]
+    },
+    {
+        "name": "M & P Biancamano",
+        "address": "1116 Washington St, Hoboken, NJ 07030",
+        "categories": [
+            "Deli", "Takeout", "Italian"
+        ]
+    },
+    {
+        "name": "Alfalfa",
+        "address": "1110 Washington St, Hoboken, NJ 07030",
+        "categories": [
+            "Takeout", "Cafe", "Coffee"
+        ]
+    },
+    {
+        "name": "Augustino’s",
+        "address": "1104 Washington St, Hoboken, NJ 07030",
+        "categories": [
+            "Italian", "Takeout", "Fish", "Dine"
+        ]
+    },
+    {
+        "name": "Sirenetta Seafood & Raw Bar",
+        "address": "1039 Washington St, Hoboken, NJ 07030",
+        "categories": [
+            "Seafood", "Takeout", "Bar", "Dine","Fish","Raw"
+        ]
+    },
+    {
+        "name": "Jungle Juice 10th Street",
+        "address": "1016 Washington St, Hoboken, NJ 07030",
+        "categories": [
+            "Juice", "Takeout", "Acai", "Cafe"
+        ]
+    },
+    {
+        "name": "Asia Sushi & Chinese",
+        "address": "926 Washington St, Hoboken, NJ 07030",
+        "categories": [
+            "Asian", "Takeout", "Sushi", "Chinese"
+        ]
+    },
+    {
+        "name": "Amanda’s",
+        "address": "908 Washington St, Hoboken, NJ 07030",
+        "categories": [
+            "American", "Takeout", "Early"
+        ]
+    },
+    {
+        "name": "Bagels On the Hudson",
+        "address": "802 Washington St, Hoboken, NJ 07030",
+        "categories": [
+            "Breakfast", "Takeout", "Bagel"
+        ]
+    },
+    {
+        "name": "8th Street Taven",
+        "address": "800 Washington St, Hoboken, NJ 07030",
+        "categories": [
+            "Grill", "Takeout", "Bar"
+        ]
+    },
+    {
+        "name": "Margherita’s",
+        "address": "740 Washington St, Hoboken, NJ 07030",
+        "categories": [
+            "Cocktail", "Takeout", "Pizza", "Italian"
+        ]
+    },
+    {
+        "name": "Pita Pit",
+        "address": "732 Washington St, Hoboken, NJ 07030",
+        "categories": [
+            "Sandwich", "Takeout", "Deli"
+        ]
+    },
+    {
+        "name": "Hoboken Hot Bagels",
+        "address": "634 Washington St, Hoboken, NJ 07030",
+        "categories": [
+            "Bagel", "Takeout"
+        ]
+    },
+    {
+        "name": "Kung Fu Tea",
+        "address": "536 Washington St, Hoboken, NJ 07030",
+        "categories": [
+            "Tea", "Coffee", "Cafe"
+        ]
+    },
+    {
+        "name": "Grimaldi’s Pizzeria",
+        "address": "411 Washington St, Hoboken, NJ 07030",
+        "categories": [
+            "Pizza", "Takeout", "Dine"
+        ]
+    },
+    {
+        "name": "The Cuban",
+        "address": "333 Washington St, Hoboken, NJ 07030",
+        "categories": [
+            "Cuban", "Bar", "Comfort"
+        ]
+    },
+    {
+        "name": "Mamoun’s Falafel",
+        "address": "300 Washington St, Hoboken, NJ 07030",
+        "categories": [
+            "Middle Eastern", "Takeout"
+        ]
+    },
+    {
+        "name": "Tally-Ho",
+        "address": "526 Washington St, Hoboken, NJ 07030",
+        "categories": [
+            "Bar", "Takeout", "Comfort"
+        ]
+    },
+    {
+        "name": "La Bouche",
+        "address": "526 Washington St, Hoboken, NJ 07030",
+        "categories": [
+            "Breakfast", "Takeout", "Brunch", "Coffee"
+        ]
+    },
+    {
+        "name": "The Brass Rail",
+        "address": "135 Washington St, Hoboken, NJ 07030",
+        "categories": [
+            "Bar", "Takeout", "American", "Dine"
+        ]
+    },
+    {
+        "name": "honeygrow",
+        "address": "120 Washington St, Hoboken, NJ 07030",
+        "categories": [
+            "Salads", "Light", "Healthy", "Locally Sourced"
+        ]
+    },
+    {
+        "name": "Johnny Rockets",
+        "address": "134 Washington St, Hoboken, NJ 07030",
+        "categories": [
+            "Retro", "Takeout", "American", "Dine"
+        ]
+    },
+    {
+        "name": "Cluck U Chicken",
+        "address": "112 Washington St, Hoboken, NJ 07030",
+        "categories": [
+            "Chicken", "Fast Food"
+        ]
+    },
+    {
+        "name": "Empanadas Cafe",
+        "address": "123 Washington St, Hoboken, NJ 07030",
+        "categories": [
+            "Cafe", "South American", "Coffee"
+        ]
+    },
+    {
+        "name": "La Isla",
+        "address": "104 Washington St, Hoboken, NJ 07030",
+        "categories": [
+            "Cuban", "Comfort", "BYOB"
+        ]
+    },
+    {
+        "name": "Carlo’s Bakery",
+        "address": "95 Washington St, Hoboken, NJ 07030",
+        "categories": [
+            "Bakery", "Takeout"
+        ]
+    },
+    {
+        "name": "Bluestone Lane",
+        "address": "409 Washington St, Hoboken, NJ 07030",
+        "categories": [
+            "Cafe", "Coffee"
+        ]
     }
 ]
-
 
 //Returns: Array of strings
 //Purpose: Parse the individual words in the given string
@@ -50,7 +294,6 @@ function checkInJSON(str, index){
 }
 
 function getInput(){
-
     string = document.getElementById("SearchBar").value;
     stringArray = parseInput(string);
     
@@ -109,3 +352,68 @@ function loadAllRestaurants(){
         cell2.innerHTML = elmAddress;
     }
 }
+
+function collapseNav(){
+    navButton = document.getElementById("navButton");
+    sidebar = document.getElementById("nav");
+
+    navButton.style.display = "block";
+    sidebar.style.display = "none";
+
+    expanded = false;
+}
+
+function expandNav(){
+    navButton = document.getElementById("navButton");
+    sidebar = document.getElementById("nav");
+
+    navButton.style.display = "none";
+    sidebar.style.display = "block";
+
+    expanded = true;
+}
+
+inMobile = false;
+expanded = true;
+window.onresize = checkWindowState;
+
+
+function checkWindowState() {
+    main = document.getElementById("main");
+    title = document.getElementById("mainTitle");
+    if (window.innerWidth < 900){
+        inMobile = true;
+        main.style.marginLeft = "5%";
+        main.style.marginRight = "5%";
+        title.style.fontSize = "25px";
+    }
+    else {
+        inMobile = false;
+        main.style.marginLeft = "25%";
+        main.style.marginRight = "25%";
+        title.style.fontSize = "2.76vw";
+    }
+
+    if (inMobile){
+        collapseNav();
+    }
+    else {
+        expandNav();
+    }
+}
+function updateNav() {
+    if (inMobile && expanded==true){
+        console.log(event.mouseX);
+        if (event.clientX > 200){
+            collapseNav();
+        }
+    }
+}
+
+window.addEventListener("click", function(){
+    updateNav();
+})
+
+document.addEventListener("DOMContentLoaded", function(){
+    checkWindowState();
+})
